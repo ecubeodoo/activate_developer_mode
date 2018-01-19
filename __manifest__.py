2537be32c4cdd77505992c547db931c713982ff6
rocky <ehtishamfaisal@gmail.com>
2018-01-19 17:32:16 +0500

    [ADD] Resouces added developer mode

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+{
+    'name' : 'Activate Developer Mode',
+    'version' : '10.0',
+    'author' : 'Ehtisham Faisal',
+    'website' : 'http://www.oxenlab.com',
+    "category": "Tools",
+    'summary': 'Activate Developer Mode for developement purposes',
+    'description': """
+""",
+    'depends' : ['base'],
+    'data': [
+    ],
+    'installable': True
+}
